744bf11be079f9be5e567d1c1b2a5d2b27d91ee3
xvivex <vivek.dalvi333@gmail.com>
2023-08-19 04:25:07 +0530

    latest

--- src/main/java/com/codereview/AssignmentSubmissionApp/AssignmentSubmissionApplication.java
+++ src/main/java/com/codereview/AssignmentSubmissionApp/AssignmentSubmissionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentSubmissionAppApplication {
+public class AssignmentSubmissionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentSubmissionAppApplication.class, args);
+		SpringApplication.run(AssignmentSubmissionApplication.class, args);
 	}
 
 }
--- src/main/java/com/codereview/AssignmentSubmissionApp/ServletInitializer.java
+++ src/main/java/com/codereview/AssignmentSubmissionApp/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AssignmentSubmissionAppApplication.class);
+		return application.sources(AssignmentSubmissionApplication.class);
 	}
 
 }
